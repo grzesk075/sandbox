f4da7d4807a0fa109616e5138c5f3c86d71510f2
Grzegorz Kuprianowicz <grzegorz.kuprianowicz@tomtom.com>
2019-07-12 15:55:13 +0200

    Big O complexity.

---
+++ src/main/java/pl/grzesk075/sandbox/codility/complexity/BigO.java
@@ -0,0 +1,14 @@
+package pl.grzesk075.sandbox.codility.complexity;
+
+/**
+ * Time complexity depends on that, how increases number of dominant operations,
+ * accordingly to increasing input data size.
+ * Worst-case examples should be considered during measuring/estimation the complexity.
+ * Time complexity is described in Big-O notation.
+ * O(1)         constant            n % 2
+ * O(n)         linear              for i in range(n): a *= i
+ * O(log n)     logarithmic         while n > 1: n // 2; result += 1
+ *
+ */
+public class BigO {
+}
