1539edaa289d59a6d58f8d13978f6f5fecb5dff2
Grzegorz Kuprianowicz <grzesk075@gmail.com>
2019-07-13 17:36:18 +0200

    Time and space complexity.

--- src/main/java/pl/grzesk075/sandbox/codility/complexity/BigO.java
+++ src/main/java/pl/grzesk075/sandbox/codility/complexity/BigO.java
@@ -6,9 +6,19 @@ package pl.grzesk075.sandbox.codility.complexity;
  * Worst-case examples should be considered during measuring/estimation the complexity.
  * Time complexity is described in Big-O notation.
  * O(1)         constant            n % 2
- * O(n)         linear              for i in range(n): a *= i
  * O(log n)     logarithmic         while n > 1: n // 2; result += 1
+ * O(n)         linear              for i in range(n): a *= i
+ * O(n+m)       linear              for i in range(n): a *= i; for j in range(m): a *= j
+ * O(n log n)
+ * O(n^2)       quadratic           for i in range(n): for j in range(n): a *= j
+ * O(n!)        factorial
+ * O(2^n)       expotential
  *
+ * Space complexity depends of number of variables to be declared (stack in case of recursion and other memory as well).
+ * O(1) is for constant number of variables.
+ * O(n) is if you have to declare array of length n.
  */
 public class BigO {
+
+    /** Calculate sum of that series 1 + 2 + 3 + ... + n. */
 }
