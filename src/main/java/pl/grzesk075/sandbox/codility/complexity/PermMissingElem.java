6246cb0c7d851f044e92f539bf89e5716730f501
Grzegorz Kuprianowicz <grzesk075@gmail.com>
2019-07-18 19:47:34 +0200

    Codility PermMissingElem solution description.

--- src/main/java/pl/grzesk075/sandbox/codility/complexity/PermMissingElem.java
+++ src/main/java/pl/grzesk075/sandbox/codility/complexity/PermMissingElem.java
@@ -1,5 +1,11 @@
 package pl.grzesk075.sandbox.codility.complexity;
 
+/**
+ * N is an integer within the range [0..100,000].
+ * the elements of A are all distinct
+ * each element of array A is an integer within the range [1..(N + 1)]
+ * so one element is "missing" - return it
+ */
 public class PermMissingElem {
     class Solution {
         public int solution(int[] A) {
