a171880b1a91f333c234baf1d90bb2747074e5c0
Grzegorz Kuprianowicz <grzegorz.kuprianowicz@tomtom.com>
2019-11-15 16:28:10 +0100

    Find longest palindrome unit tests.

--- src/main/java/pl/grzesk075/sandbox/leetcode/LongestPalindrome.java
+++ src/main/java/pl/grzesk075/sandbox/leetcode/LongestPalindrome.java
@@ -1,13 +1,13 @@
 package pl.grzesk075.sandbox.leetcode;
 
 public class LongestPalindrome {
-    public int getLongestPalindromeLength(final String s){
+    public static int getLongestPalindromeLength(final String s){
         if(s.isEmpty()){
             return 0;
         }
         int longest = 1;
         for (int i = 1; i < s.length(); i++) {
-            for (int j = 0; j <=1 && i+j<s.length(); j++) {
+            for (int j = 1, shift = 0; i - j - shift > 0 && i + j - 1 < s.length(); j++) {
 
             }
         }
---
+++ src/test/java/pl/grzesk075/sandbox/leetcode/LongestPalindromeTest.java
@@ -0,0 +1,39 @@
+package pl.grzesk075.sandbox.leetcode;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LongestPalindromeTest {
+
+    @Test
+    public void shouldFindLongestPalindromeLength() {
+        assertEquals(0, LongestPalindrome.getLongestPalindromeLength(""));
+        assertEquals(1, LongestPalindrome.getLongestPalindromeLength("a"));
+        assertEquals(1, LongestPalindrome.getLongestPalindromeLength("abcdef"));
+        assertEquals(2, LongestPalindrome.getLongestPalindromeLength("aabcdef"));
+        assertEquals(2, LongestPalindrome.getLongestPalindromeLength("abbcdef"));
+        assertEquals(2, LongestPalindrome.getLongestPalindromeLength("abccdef"));
+        assertEquals(2, LongestPalindrome.getLongestPalindromeLength("abcddef"));
+        assertEquals(2, LongestPalindrome.getLongestPalindromeLength("abcdeef"));
+        assertEquals(2, LongestPalindrome.getLongestPalindromeLength("abcdeff"));
+        assertEquals(3, LongestPalindrome.getLongestPalindromeLength("aaabcdef"));
+        assertEquals(3, LongestPalindrome.getLongestPalindromeLength("abcccdef"));
+        assertEquals(3, LongestPalindrome.getLongestPalindromeLength("abcdefff"));
+        assertEquals(3, LongestPalindrome.getLongestPalindromeLength("abacdef"));
+        assertEquals(3, LongestPalindrome.getLongestPalindromeLength("abcbdef"));
+        assertEquals(3, LongestPalindrome.getLongestPalindromeLength("abcdcef"));
+        assertEquals(3, LongestPalindrome.getLongestPalindromeLength("abcdedf"));
+        assertEquals(3, LongestPalindrome.getLongestPalindromeLength("abcdefe"));
+        assertEquals(4, LongestPalindrome.getLongestPalindromeLength("abbacdef"));
+        assertEquals(4, LongestPalindrome.getLongestPalindromeLength("abcddcef"));
+        assertEquals(4, LongestPalindrome.getLongestPalindromeLength("abcdeffe"));
+        assertEquals(5, LongestPalindrome.getLongestPalindromeLength("abcbadef"));
+        assertEquals(5, LongestPalindrome.getLongestPalindromeLength("abcdcbef"));
+        assertEquals(5, LongestPalindrome.getLongestPalindromeLength("abcdefed"));
+        assertEquals(6, LongestPalindrome.getLongestPalindromeLength("abccba"));
+        assertEquals(6, LongestPalindrome.getLongestPalindromeLength("sdfabccbarty"));
+        assertEquals(6, LongestPalindrome.getLongestPalindromeLength("abccbaert"));
+        assertEquals(6, LongestPalindrome.getLongestPalindromeLength("werabccba"));
+    }
+}
