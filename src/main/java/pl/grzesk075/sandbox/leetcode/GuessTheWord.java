d49f41b477f8ad4ec6e0b9ffafbc6b9f7751f9c0
Grzegorz Kuprianowicz <grzesk075@gmail.com>
2019-12-29 20:26:21 +0100

    LeetCode GuessTheWord interactive problem requiring minimax with heuristic.

--- src/main/java/pl/grzesk075/sandbox/leetcode/GuessTheWord.java
+++ src/main/java/pl/grzesk075/sandbox/leetcode/GuessTheWord.java
@@ -1,5 +1,10 @@
 package pl.grzesk075.sandbox.leetcode;
 
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 public class GuessTheWord {
 
     public static final int WORD_LENGTH = 6;
@@ -11,19 +16,18 @@ public class GuessTheWord {
     }
 
     public void findSecretWord(String[] wordlist, Master master) {
-        String wordToCheck = null;
-        for (int i = 0; i < wordlist.length; i++) {
-            wordToCheck = wordlist[i];
-            if (wordToCheck != null) {
-                wordlist[i] = null;
-                break;
-            }
-        }
+        List<String> words = new LinkedList<>();
+        Collections.addAll(words, wordlist);
+        findSecretWord(words, master);
+    }
+
+    public void findSecretWord(List<String> wordlist, Master master) {
+        final String wordToCheck = getMinMaxWord(wordlist);
         if (wordToCheck == null) {
             return;
         }
         final int guess = master.guess(wordToCheck);
-        System.out.println("Guess " + guess);
+        System.out.println("Guess " + guess);  // 0 is most likely
         if (guess == IS_SECRET_WORD) {
             System.out.println("Secret word is: " + wordToCheck);
             return;
@@ -33,19 +37,47 @@ public class GuessTheWord {
         findSecretWord(wordlist, master);
     }
 
-    private void removeNotMatchingWords(String[] wordlist, String wordToCheck, int guess) {
-        final boolean noOneLetterMatches = guess == NO_ONE_LETTER_METCHES;
-        for (int i = 0; i < wordlist.length; i++) {
-            final String word = wordlist[i];
-            if (word == null) {
-                continue;
+    private String getMinMaxWord(List<String> wordlist) {
+        final int length = wordlist.size();
+        if (length == 0) {
+            return null;
+        }
+        if (length < 3) {
+            return wordlist.get(0);
+        }
+        //final int[][] matchMatrix = new int[length][length];
+        final int[] minmaxScore = new int[length];
+        for (int i = 1; i < length; i++) {
+            for (int j = 0; j < i; j++) {
+                final int match = match(wordlist.get(i), wordlist.get(j));
+                //matchMatrix[i][j] = match;
+                minmaxScore[i] += match;
+                minmaxScore[j] += match;
             }
+        }
+        final int indexOfMinMaxWord = getIndexOfLargest(minmaxScore);
+        return wordlist.remove(indexOfMinMaxWord);
+    }
+
+    private void removeNotMatchingWords(List<String> wordlist, String wordToCheck, int guess) {
+        final boolean noOneLetterMatches = guess == NO_ONE_LETTER_METCHES;
+        final Iterator<String> iterator = wordlist.iterator();
+        while (iterator.hasNext()) {
+            String word = iterator.next();
             final int match = match(word, wordToCheck);
             final boolean removeWord = noOneLetterMatches ? match > 0 : match != guess;
             if (removeWord) {
-                wordlist[i] = null;
+                iterator.remove();
             }
         }
+    }
+
+    static int getIndexOfLargest(int[] array) {
+        int maxAt = 0;
+        for (int i = 0; i < array.length; i++) {
+            maxAt = array[i] > array[maxAt] ? i : maxAt;
+        }
+        return maxAt;
     }
 
     static int match(String word, String wordToCheck) {
