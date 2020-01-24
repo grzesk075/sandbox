f6672057172e378053780a00638dbbf3cb1e648e
Grzegorz Kuprianowicz <grzegorz.kuprianowicz@tomtom.com>
2020-01-24 16:52:11 +0100

    Describe java 13 features.

--- src/main/java/pl/grzesk075/sandbox/java/Java13Tutorial.java
+++ src/main/java/pl/grzesk075/sandbox/java/Java13Tutorial.java
@@ -1,16 +1,25 @@
 package pl.grzesk075.sandbox.java;
 
 /**
- * New major features in JAVA 13.
+ * New features in JAVA 13.
  */
 public class Java13Tutorial {
 
     public static void main(String[] args) {
 
-        System.out.println("New major features in JAVA 13.");
+        System.out.println("New features in JAVA 13.");
 
         /*
-        1.
+        1. JEP 350: Dynamic CDS Archives
+        Static Class Data Sharing is still available.
+        Java system classes are placed in classes.jsa instead of rt.jar.
+        Loading from jsa file is faster and memory footprint is lower.
+        Jsa file has closer format to metaspace structure in java heap, where classes definitions are stored.
+        Multiple JVM shares loaded classes.
+
+        Using Dynamic Class Data Sharing, you can dump your classes to jsa file and use later with the same benefits.
+        java -XX:ArchiveClassesAtExit=hello.jsa -cp hello.jar Hello
+        java -XX:SharedArchiveFile=hello.jsa -cp hello.jar Hello
         */
     }
 }
