245b40d0878fa538a749e60c8c6bb5d2de0d22c0
Grzegorz Kuprianowicz <grzegorz.kuprianowicz@tomtom.com>
2020-04-16 15:42:17 +0200

    Leetcode delete node.

---
+++ src/main/java/pl/grzesk075/sandbox/leetcode/DeleteNode.java
@@ -0,0 +1,20 @@
+package pl.grzesk075.sandbox.leetcode;
+
+/**
+ * 237. Delete Node in a Linked List.
+ */
+public class DeleteNode {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
