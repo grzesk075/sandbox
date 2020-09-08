a3453c246eb59a80b598457521369920fedccab2
Grzegorz Kuprianowicz <grzegorz.kuprianowicz@tomtom.com>
2020-09-08 14:50:48 +0200

    Copy linked list beginning.

--- src/main/java/pl/grzesk075/sandbox/google/CopyLinkedList.java
+++ src/main/java/pl/grzesk075/sandbox/google/CopyLinkedList.java
@@ -2,7 +2,7 @@ package pl.grzesk075.sandbox.google;
 
 /**
  * Copy linked list with arbitrary pointer.
- *
+ * <p/>
  * Problem statement:
  * You are given a linked list where the node has two pointers. The first is the regular ‘next’ pointer.
  * The second pointer is called ‘arbitrary_pointer’ and it can point to any node in the linked list.
@@ -11,4 +11,32 @@ package pl.grzesk075.sandbox.google;
  * should not affect the copied list.
  */
 public class CopyLinkedList {
+
+    public static Node deepCopyLinkedList(Node head) {
+        Node copyHead = null;
+        Node copyNode = null;
+        Node node = head;
+        while (node != null) {
+            Node newNode = new Node(node.value);
+            if (copyHead == null) {
+                copyHead = newNode;
+                copyNode = newNode;
+            } else {
+                copyNode.next = newNode;
+                copyNode = newNode;
+            }
+            node = node.next;
+        }
+        return copyHead; //TODO: shallow copy so far
+    }
+
+    public static class Node {
+        Node next;
+        Node arbitrary_pointer;
+        int value;
+
+        public Node(final int value) {
+            this.value = value;
+        }
+    }
 }
