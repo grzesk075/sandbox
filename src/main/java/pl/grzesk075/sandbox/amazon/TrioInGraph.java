05fe808f6e8f93f0d2d8628607352b33af5271c3
Grzegorz Kuprianowicz <grzesk075@gmail.com>
2020-10-02 14:57:13 +0200

    Trio in graph.

---
+++ src/main/java/pl/grzesk075/sandbox/amazon/TrioInGraph.java
@@ -0,0 +1,12 @@
+package pl.grzesk075.sandbox.amazon;
+
+/**
+ * Find cycles in graph containing 3 nodes (trio).
+ * Count sum (amount) of nodes for each trio, that are directly connected with trio.
+ * Return the biggest sum, otherwise return -1, if there are no trio in graph.
+ */
+public class TrioInGraph {
+    public static int getBiggestSum(int numberOfNodes, int[] fromNodes, int[] toNodes){
+        return -1;
+    }
+}
