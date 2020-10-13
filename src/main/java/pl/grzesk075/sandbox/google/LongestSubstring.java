9aa06b7216a330441925b396ab175807be0a521d
Grzegorz Kuprianowicz <grzesk075@gmail.com>
2020-10-13 15:38:40 +0200

    Longest substring.

--- src/main/java/pl/grzesk075/sandbox/google/LongestSubstring.java
+++ src/main/java/pl/grzesk075/sandbox/google/LongestSubstring.java
@@ -2,9 +2,14 @@ package pl.grzesk075.sandbox.google;
 
 /**
  * Longest substring with no more than ‘k’ distinct characters.
- *
+ * <p>
  * Problem statement:
  * Given a string, find the length of the longest substring in it with no more than K distinct characters.
+ * <p>
+ * Caterpillar method O(n).
  */
 public class LongestSubstring {
+    public static int getLongestSubstringLength(String s, int k) {
+
+    }
 }
---
+++ src/test/java/pl/grzesk075/sandbox/google/LongestSubstringTest.java
@@ -0,0 +1,20 @@
+package pl.grzesk075.sandbox.google;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LongestSubstringTest {
+
+    @Test
+    public void getLongestSubstringLengthTest() {
+        assertEquals(0, LongestSubstring.getLongestSubstringLength("", 5));
+        assertEquals(1, LongestSubstring.getLongestSubstringLength("a", 5));
+        assertEquals(3, LongestSubstring.getLongestSubstringLength("abc", 5));
+        assertEquals(4, LongestSubstring.getLongestSubstringLength("abac", 5));
+        assertEquals(6, LongestSubstring.getLongestSubstringLength("xfabacxf", 5));
+        assertEquals(15, LongestSubstring.getLongestSubstringLength("abcdefadcbbaaeeeabcdeffffffffff", 5));
+        assertEquals(15, LongestSubstring.getLongestSubstringLength("adcbbaaeeeabcdeffffffffffabcdef", 5));
+        assertEquals(15, LongestSubstring.getLongestSubstringLength("ffffffffffabcdefadcbbaaeeeabcde", 5));
+    }
+}
