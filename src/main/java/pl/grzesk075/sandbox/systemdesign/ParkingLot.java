6f481d870d0c7781c9c9e492127e6b0e0cfda874
Grzegorz Kuprianowicz <grzegorz.kuprianowicz@tomtom.com>
2020-10-26 15:58:05 +0100

    Design a Parking Lot.

--- src/main/java/pl/grzesk075/sandbox/systemdesign/ParkingLot.java
+++ src/main/java/pl/grzesk075/sandbox/systemdesign/ParkingLot.java
@@ -1,5 +1,12 @@
 package pl.grzesk075.sandbox.systemdesign;
 
+enum Size {
+    CAR,
+    VAN,
+    BUS,
+    TRUCK
+}
+
 /**
  * Design a Parking Lot.
  * 1. Handling ambiguity. Ask general questions.
@@ -10,5 +17,38 @@ package pl.grzesk075.sandbox.systemdesign;
  * Preferable is O(1) - big o (ou) of one.
  * Design dictionaries in enums and data flow in functions.
  */
-public class ParkingLot {
+public class ParkingLot {  // Singleton
+    Spot placeVehicle(Vehicle vehicle) {
+    }
+
+    void removeVehicle(Vehicle vehicle) {
+    }
 }
+
+class Spot {
+    int id;
+    Size size;
+}
+
+class Vehicle {
+    String licensePlate;
+    Size size;
+}
+
+class Gate {
+    int id;
+}
+
+class Receipt {
+    long id;
+}
+
+class Ticket {
+    long id;
+}
+
+class CashRegister {
+    long id;
+}
+
+//Optionally sensors of vehicle with lights to show route from gate to spot and vice versa.
