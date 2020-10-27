0543b102a7f1a57308fde43e15fbc89cac018845
Grzegorz Kuprianowicz <grzegorz.kuprianowicz@tomtom.com>
2020-10-27 14:48:19 +0100

    Design a Parking Lot.

--- src/main/java/pl/grzesk075/sandbox/systemdesign/ParkingLot.java
+++ src/main/java/pl/grzesk075/sandbox/systemdesign/ParkingLot.java
@@ -1,5 +1,10 @@
 package pl.grzesk075.sandbox.systemdesign;
 
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
 enum Size {
     CAR,
     VAN,
@@ -17,11 +22,35 @@ enum Size {
  * Preferable is O(1) - big o (ou) of one.
  * Design dictionaries in enums and data flow in functions.
  */
-public class ParkingLot {  // Singleton
-    Spot placeVehicle(Vehicle vehicle) {
+public class ParkingLot {
+
+    Map<Size, List<Spot>> freeSpots; //persistent hash map
+
+    Map<Vehicle, Spot> occupiedSpots; //persistent hash map
+
+    List<Gate> gates;
+
+    List<CashRegister> cashRegisters;
+
+    List<Receipt> archivedReceipts; //persistent list with retention policy
+
+    public ParkingLot(Path config) {
+    }
+
+    synchronized Spot placeVehicle(Vehicle vehicle) {
+        final List<Spot> spots = freeSpots.get(vehicle.size);
+        if (spots.isEmpty()) {
+            throw new RuntimeException("No available slots of this size.");
+        }
+        final Spot spot = spots.remove(0);
+        occupiedSpots.put(vehicle, spot);
+        return spot;
     }
 
     void removeVehicle(Vehicle vehicle) {
+        final Spot spot = occupiedSpots.remove(vehicle);
+        final List<Spot> spots = freeSpots.get(vehicle.size);
+        spots.add(spot);
     }
 }
 
@@ -33,22 +62,58 @@ class Spot {
 class Vehicle {
     String licensePlate;
     Size size;
+
+    public Vehicle(final String licensePlate, final Size size) {
+
+    }
+
+    //hashCode, equals using natural composite key
 }
 
 class Gate {
     int id;
+    ParkingLot parkingLot;
+
+    Ticket placeVehicle(String licensePlate, Size size) {
+        final Spot spot = parkingLot.placeVehicle(new Vehicle(licensePlate, size));
+        return new Ticket();
+    }
+
+    void releaseVehicle(Ticket ticket) {
+        if (ticket.paid) {
+            parkingLot.removeVehicle(ticket.vehicle);
+        } else {
+            //Ticket not paid.
+        }
+    }
 }
 
 class Receipt {
     long id;
+    LocalDateTime issueTime;
+    LocalDateTime fromTime;
+    LocalDateTime toTime;
+    String licensePlate;
+    int price; //dollars
+    byte priceFraction; //cents
+    String isoCurrencyCode;  //ISO 4217 currency code
 }
 
 class Ticket {
     long id;
+    boolean paid;
+    Vehicle vehicle;
+    String customerInfo;
 }
 
 class CashRegister {
-    long id;
+    int id;
+    Tariff tariff;
+}
+
+class Tariff {
+    String isoCurrencyCode;  //ISO 4217 currency code
+    Object priceList;
 }
 
 //Optionally sensors of vehicle with lights to show route from gate to spot and vice versa.
