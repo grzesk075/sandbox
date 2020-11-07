87ec40ece3d8050020c9341b529bc6c4a9770f1f
Grzegorz Kuprianowicz <grzesk075@gmail.com>
2020-11-07 21:50:10 +0100

    Algo Expert first example.

---
+++ src/main/java/pl/grzesk075/sandbox/algoexpert/MaxProfitWithKTransactions.java
@@ -0,0 +1,8 @@
+package pl.grzesk075.sandbox.algoexpert;
+
+public class MaxProfitWithKTransactions {
+    public static int maxProfitWithKTransactions(int[] prices, int k) {
+        // Write your code here.
+        return -1;
+    }
+}
