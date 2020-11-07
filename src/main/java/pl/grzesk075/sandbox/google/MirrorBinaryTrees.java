d05cc68946e7477cbf218c1c4a8523b0ce6524e8
Grzegorz Kuprianowicz <grzesk075@gmail.com>
2020-11-07 19:40:01 +0100

    Mirror binary tree.

--- src/main/java/pl/grzesk075/sandbox/google/MirrorBinaryTrees.java
+++ src/main/java/pl/grzesk075/sandbox/google/MirrorBinaryTrees.java
@@ -1,8 +1,12 @@
 package pl.grzesk075.sandbox.google;
 
+import pl.grzesk075.sandbox.leetcode.InvertBinaryTree;
+
 /**
  * Mirror binary trees.
- * <p></p>
+ * <p>
+ * See {@link InvertBinaryTree}.
+ * <p>
  * Problem statement:
  * Given the root node of a binary tree, swap the ‘left’ and ‘right’ children for each node.
  */
