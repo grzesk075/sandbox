0b38da76534b12a5fb941838d61d73afc32452aa
Grzegorz Kuprianowicz <grzesk075@gmail.com>
2021-02-20 19:05:35 +0100

    Weather measurement system.

--- src/main/java/pl/grzesk075/sandbox/systemdesign/WeatherMeasurementSystem.java
+++ src/main/java/pl/grzesk075/sandbox/systemdesign/WeatherMeasurementSystem.java
@@ -1,5 +1,8 @@
 package pl.grzesk075.sandbox.systemdesign;
 
+import java.time.ZonedDateTime;
+import java.util.List;
+
 /**
  * Sensors [value, unit, timestamp].
  * Concentrators [asking sensors, short term accumulation, aggregation e.g. 4 average measurements per hour].
@@ -9,4 +12,49 @@ package pl.grzesk075.sandbox.systemdesign;
  * Redundancy.
  */
 public class WeatherMeasurementSystem {
+
+    enum Unit {
+        PREASURE,
+        HUMIDITY,
+        TEMPERATURE;
+    }
+
+    class Measurement {
+        long id;
+        long sensorId;
+        Unit unit;
+        Float value;
+        ZonedDateTime zonedDateTime;
+    }
+
+    class Sensor {
+        List<Measurement> measurements; //size is limited
+
+        List<Measurement> getMeasurements(long lastMeasurementId) {
+            return measurements; //newer should be filtered
+        }
+    }
+
+    class Concentrator {
+        List<Sensor> sensors;
+
+        List<Measurement> shortTermMeasurements;
+
+        void getNewerMeasurements() {
+            for (Sensor s : sensors) {
+                //s.getMeasurements();
+            }
+        } //runs periodically
+    }
+
+    class DataCenter {
+        List<Concentrator> concentrators;
+        Database db; //relational
+        //dashboards, prognosis, analysis, reports, warnings, health check for broken sensors and connections
+    }
+
+    class Database {
+        List<Measurement> shortTermMeasurements; //higher density of measurements in time
+        List<Measurement> longTermMeasurements;
+    }
 }
